0831abb51db0896fd7000bf90e1d5e3307202674
Brinda Vallat <brinda.vallat@rcsb.org>
2021-01-29 18:45:11 -0500

    Model updates

--- config-scripts/schema-updates/ciff_extension/2021_01_model_updates_bv.py
+++ config-scripts/schema-updates/ciff_extension/2021_01_model_updates_bv.py
@@ -209,6 +209,187 @@ def define_tdoc_ihm_pseudo_site():
     
     return table_def
 
+#====================================================
+def define_tdoc_ihm_cross_link_pseudo_site():
+    table_name='ihm_cross_link_pseudo_site'
+    comment='Details of pseudo sites involved in crosslinks'
+
+    column_defs = [
+        Column.define(
+            "id",
+            builtin_types.int8,
+            comment='An identifier for a pseudo site involved in a crosslink',
+            nullok=False
+        ),
+        Column.define(
+            "restraint_id",
+            builtin_types.int8,
+            comment='An identifier for the crosslink restraint between a pair of residues',
+            nullok=False
+        ),
+        Column.define(
+            "cross_link_partner",
+            builtin_types.int8,
+            comment='The identity of the crosslink partner corresponding to the pseudo site',
+            nullok=False
+        ),
+        Column.define(
+            "pseudo_site_id",
+            builtin_types.int8,
+            comment='The pseudo site identifier corresponding to the crosslink partner',
+            nullok=False
+        ),
+        Column.define(
+            "model_id",
+            builtin_types.int8,
+            comment='Identifier to the model that the pseudo site corresponds to',
+            nullok=True
+        ),
+        Column.define(
+            "structure_id",
+            builtin_types.text,
+            comment='Structure identifier',
+            nullok=False
+        )
+    ]
+
+    key_defs = [
+        Key.define(["id"], constraint_names=[["PDB", "ihm_cross_link_pseudo_site_id_key"]] ),
+        Key.define(["RID"], constraint_names=[["PDB", "ihm_cross_link_pseudo_site_RID_key"]] ),
+    ]
+
+    # @brinda: add fk pseudo-definition
+    fkey_defs = [
+        ForeignKey.define(["structure_id"], "PDB", "entry", ["id"],
+                          constraint_names=[["PDB", "ihm_cross_link_pseudo_site_structure_id_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        ),
+        ForeignKey.define(["restraint_id"], "PDB", "ihm_cross_link_restraint", ["id"],
+                          constraint_names=[["PDB", "ihm_cross_link_pseudo_site_restraint_id_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        ),
+        ForeignKey.define(["model_id"], "PDB", "ihm_model_list", ["model_id"],
+                          constraint_names=[["PDB", "ihm_cross_link_pseudo_site_model_id_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        ),
+        ForeignKey.define(["cross_link_partner"], "Vocab", "cross_link_partner", ["Name"],
+                          constraint_names=[ ["Vocab", "ihm_cross_link_pseudo_site_cross_link_partner_fkey"] ],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION")
+
+    ]
+
+    table_def = Table.define(
+        table_name,
+        column_defs,
+        key_defs=key_defs,
+        fkey_defs=fkey_defs,
+        comment=comment,
+        provide_system=True
+    )
+
+    return table_def
+
+#====================================================
+def define_tdoc_ihm_ensemble_sub_sample():
+    table_name='ihm_ensemble_sub_sample'
+    comment='Details of the sub samples within the ensembles'
+
+    column_defs = [
+        Column.define(
+            "id",
+            builtin_types.int8,
+            comment='A unique id for the ensemble sub sample',
+            nullok=False
+        ),
+        Column.define(
+            "sample_name",
+            builtin_types.text,
+            comment='A name for the ensemble sub sample',
+            nullok=True
+        ),
+        Column.define(
+            "ensemble_id",
+            builtin_types.int8,
+            comment='The ensemble identifier corresponding to the sub sample',
+            nullok=False
+        ),
+        Column.define(
+            "num_models",
+            builtin_types.int8,
+            comment='The number of models in the ensemble sub sample',
+            nullok=False
+        ),
+        Column.define(
+            "num_models_deposited",
+            builtin_types.int8,
+            comment='The number of models in the sub sample that are deposited',
+            nullok=True
+        ),
+        Column.define(
+            "model_group_id",
+            builtin_types.int8,
+            comment='The model group identifier corresponding to the sub sample',
+            nullok=True
+        ),
+        Column.define(
+            "file_id",
+            builtin_types.int8,
+            comment='A reference to the external file containing the structural models in the sub sample',
+            nullok=True
+        ),
+        Column.define(
+            "structure_id",
+            builtin_types.text,
+            comment='Structure identifier',
+            nullok=False
+        )
+    ]
+
+    key_defs = [
+        Key.define(["id"], constraint_names=[["PDB", "ihm_ensemble_sub_sample_id_key"]] ),
+        Key.define(["RID"], constraint_names=[["PDB", "ihm_ensemble_sub_sample_RID_key"]] ),
+    ]
+
+    # @brinda: add fk pseudo-definition
+    fkey_defs = [
+        ForeignKey.define(["structure_id"], "PDB", "entry", ["id"],
+                          constraint_names=[["PDB", "ihm_ensemble_sub_sample_structure_id_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        ),
+        ForeignKey.define(["ensemble_id"], "PDB", "ihm_ensemble_info", ["ensemble_id"],
+                          constraint_names=[["PDB", "ihm_ensemble_sub_sample_ensemble_id_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        ),
+        ForeignKey.define(["model_group_id"], "PDB", "ihm_model_group", ["id"],
+                          constraint_names=[["PDB", "ihm_ensemble_sub_sample_model_group_id_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        ),
+        ForeignKey.define(["file_id"], "PDB", "ihm_external_files", ["id"],
+                          constraint_names=[["PDB", "ihm_ensemble_sub_sample_file_id_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        )
+
+    ]
+
+    table_def = Table.define(
+        table_name,
+        column_defs,
+        key_defs=key_defs,
+        fkey_defs=fkey_defs,
+        comment=comment,
+        provide_system=True
+    )
+
+    return table_def
+
 # ===================================================
 # update existing table
 
@@ -268,6 +449,38 @@ def update_PDB_ihm_cross_link_restraint(model):
                           on_delete="NO ACTION")
     )
     
+# ---------------
+def update_PDB_ihm_ensemble_info(model):
+    table = model.schemas['PDB'].tables['ihm_ensemble_info']
+
+    # Add the PDB.ihm_ensemble_info.sub_sample_flag and PDB.ihm_ensemble_info.sub_sampling_type columns   
+    table.create_column(
+        Column.define(
+            "sub_sample_flag",
+            builtin_types.text,
+            comment='A flag that indicates whether the ensemble consists of sub samples',
+            nullok=True
+        ),
+        Column.define(
+            "sub_sampling_type",
+            builtin_types.text,
+            comment='Type of sub sampling',
+            nullok=True
+        )
+    )
+
+    # Create the foreign keys
+    table.create_fkey(
+        ForeignKey.define(["sub_sample_flag"], "Vocab", "sub_sample_flag", ["Name"],
+                          constraint_names=[ ["Vocab", "ihm_ensemble_info_sub_sample_flag_fkey"] ],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"),
+        ForeignKey.define(["sub_sampling_type"], "Vocab", "sub_sampling_type", ["Name"],
+                          constraint_names=[ ["Vocab", "ihm_ensemble_info_sub_sampling_type_fkey"] ],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION")
+    )
+
 # ========================================================
 # add rows to Vocab.ihm_cross_link_list_linker_type table
 def add_rows_to_Vocab_ihm_cross_link_list_linker_type(catalog):
@@ -297,6 +510,49 @@ def add_rows_to_Vocab_pseudo_site_flag(catalog):    #@serban to review
     pseudo_site_flag = schema.pseudo_site_flag
     pseudo_site_flag.insert(rows, defaults=['ID', 'URI'])
 
+# -----------------------------------
+# add rows to Vocab.sub_sample_flag
+def add_rows_to_Vocab_sub_sample_flag(catalog):    #@serban to review
+
+    rows =[
+        {'Name': 'Yes', 'Description': 'Ensemble consists of sub samples'},
+        {'Name': 'No', 'Description': 'Ensemble does not consist of sub samples'}
+    ]
+    
+    pb = catalog.getPathBuilder()
+    schema = pb.Vocab
+    sub_sample_flag = schema.sub_sample_flag
+    sub_sample_flag.insert(rows, defaults=['ID', 'URI'])
+
+# -----------------------------------
+# add rows to Vocab.sub_sampling_type
+def add_rows_to_Vocab_sub_sampling_type(catalog):    #@serban to review
+
+    rows =[
+        {'Name': 'random', 'Description': 'sub samples generated by randomly partitioning all structures in the group'},
+        {'Name': 'independent', 'Description': 'each sub sample generated in the same fashion but in independent simulations'},
+        {'Name': 'other', 'Description': 'other type of sub sampling'}
+    ]
+    
+    pb = catalog.getPathBuilder()
+    schema = pb.Vocab
+    sub_sampling_type = schema.sub_sampling_type
+    sub_sampling_type.insert(rows, defaults=['ID', 'URI'])
+
+# -----------------------------------
+# add rows to Vocab.cross_link_partner
+def add_rows_to_Vocab_cross_link_partner(catalog):    #@serban to review
+
+    rows =[
+        {'Name': '1', 'Description': 'The first partner in the crosslink as identified in the ihm_cross_link_restraint table'},
+        {'Name': '2', 'Description': 'The second partner in the crosslink as identified in the ihm_cross_link_restraint table'}
+    ]
+
+    pb = catalog.getPathBuilder()
+    schema = pb.Vocab
+    cross_link_partner = schema.cross_link_partner
+    cross_link_partner.insert(rows, defaults=['ID', 'URI'])
+
 # ============================================================
 def main(server_name, catalog_id, credentials):
     server = DerivaServer('https', server_name, credentials)
@@ -305,16 +561,25 @@ def main(server_name, catalog_id, credentials):
     model = catalog.getCatalogModel()
 
     # -- create tables from scratch
-    create_table_if_not_exist(model, "PDB",  define_tdoc_ihm_pseudo_site())
-    create_table_if_not_exist(model, "Vocab",  define_Vocab_table('pseudo_site_flag', 'Flag for crosslinks involving pseudo sites'))
-    
+    #create_table_if_not_exist(model, "PDB",  define_tdoc_ihm_pseudo_site())
+    #create_table_if_not_exist(model, "Vocab",  define_Vocab_table('pseudo_site_flag', 'Flag for crosslinks involving pseudo sites'))
+    create_table_if_not_exist(model, "PDB",  define_tdoc_ihm_ensemble_sub_sample())
+    create_table_if_not_exist(model, "Vocab",  define_Vocab_table('sub_sample_flag', 'Flag for ensembles consisting of sub samples'))
+    create_table_if_not_exist(model, "Vocab",  define_Vocab_table('sub_sampling_type', 'Types of sub samples in ensembles'))
+    create_table_if_not_exist(model, "PDB",  define_tdoc_ihm_cross_link_pseudo_site())
+    create_table_if_not_exist(model, "Vocab",  define_Vocab_table('cross_link_partner', 'Identity of the crosslink partner'))
+
     # -- update existing tables
-    update_PDB_ihm_pseudo_site_feature(model)
-    update_PDB_ihm_cross_link_restraint(model)
+    #update_PDB_ihm_pseudo_site_feature(model)
+    #update_PDB_ihm_cross_link_restraint(model)
+    update_PDB_ihm_ensemble_info(model)
     
     # -- data manipulation
     #add_rows_to_Vocab_ihm_cross_link_list_linker_type(catalog)
-    add_rows_to_Vocab_pseudo_site_flag(catalog)
+    #add_rows_to_Vocab_pseudo_site_flag(catalog)
+    add_rows_to_Vocab_sub_sample_flag
+    add_rows_to_Vocab_sub_sampling_type
+    add_rows_to_Vocab_cross_link_partner
     
 # ===================================================    
 
