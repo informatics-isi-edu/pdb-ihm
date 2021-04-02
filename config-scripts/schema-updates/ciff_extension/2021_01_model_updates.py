067f474b455cce152e661da51da0e6fc5f1b1bb4
Brinda Vallat <brinda.vallat@rcsb.org>
2021-04-01 23:49:41 -0400

    Model updates

--- config-scripts/schema-updates/ciff_extension/2021_01_model_updates.py
+++ config-scripts/schema-updates/ciff_extension/2021_01_model_updates.py
@@ -680,6 +680,106 @@ def define_tdoc_ihm_data_transformation():
     
     return table_def
 
+# ========================================================
+def define_tdoc_ihm_hdx_restraint():
+    table_name='ihm_hdx_restraint'
+    comment='Details of restraint derived from hydrogen-deuterium (H/D) exchange experiments; can be uploaded as CSV/TSV file above; mmCIF category: ihm_hdx_restraint'
+
+    column_defs = [
+        Column.define(
+            "id",
+            builtin_types.int8,
+            comment='A unique id for the H/D exchange restraint',
+            nullok=False
+        ),
+        Column.define(
+            "feature_id",
+            builtin_types.int8,
+            comment='An identifier for the peptide / residue feature',
+            nullok=False
+        ),
+        Column.define(
+            "feature_RID",
+            builtin_types.text,
+            comment='Identifier to the feature RID',
+            nullok=False
+        ),
+        Column.define(
+            "protection_factor",
+            builtin_types.float8,
+            comment='The value of the protection factor determined from H/D exchange experiments',
+            nullok=True
+        ),
+        Column.define(
+            "dataset_list_id",
+            builtin_types.int8,
+            comment='Identifier to the H/D exchange input data from which the restraints are derived',
+            nullok=False
+        ),
+        Column.define(
+            "dataset_list_RID",
+            builtin_types.text,
+            comment='Identifier to the dataset list RID',
+            nullok=False
+        ),
+        Column.define(
+            "details",
+            builtin_types.text,
+            comment='Additional details regarding the H/D exchange restraint',
+            nullok=True
+        ),
+        Column.define(
+            "structure_id",
+            builtin_types.text,
+            comment='Structure identifier',
+            nullok=False
+        ),
+        Column.define(
+            "Entry_Related_File",
+            builtin_types.text,
+            comment='A reference to the uploaded restraint file in the table Entry_Related_File.id',
+            nullok=True
+        )
+    ]
+    key_defs = [
+        Key.define(["structure_id", "id"], constraint_names=[["PDB", "ihm_hdx_restraint_primary_key"]] ),
+        Key.define(["RID"], constraint_names=[["PDB", "ihm_hdx_restraint_RID_key"]] ),
+    ]
+
+    fkey_defs = [
+        ForeignKey.define(["structure_id"], "PDB", "entry", ["id"],
+                          constraint_names=[["PDB", "ihm_hdx_restraint_structure_id_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        ),
+        ForeignKey.define(["feature_RID", "structure_id", "feature_id"], "PDB", "ihm_feature_list", ["RID", "structure_id", "feature_id"],
+                          constraint_names=[["PDB", "ihm_hdx_restraint_ihm_feature_list_combo1_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        ),
+        ForeignKey.define(["dataset_list_RID", "structure_id", "dataset_list_id"], "PDB", "ihm_dataset_list", ["RID", "structure_id", "id"],
+                          constraint_names=[["PDB", "ihm_hdx_restraint_ihm_dataset_list_combo1_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        ),
+        ForeignKey.define(["Entry_Related_File"], "PDB", "Entry_Related_File", ["RID"],
+                          constraint_names=[["PDB", "ihm_hdx_restraint_Entry_Related_File_fkey"]],
+                          on_update="CASCADE",
+                          on_delete="NO ACTION"
+        )
+    ]
+
+    table_def = Table.define(
+        table_name,
+        column_defs,
+        key_defs=key_defs,
+        fkey_defs=fkey_defs,
+        comment=comment,
+        provide_system=True
+    )
+
+    return table_def
+
 #====================================================
 # update existing table
 
@@ -830,6 +930,14 @@ def update_PDB_ihm_feature_list(model):
     )
 
 # ---------------
+def update_PDB_ihm_dataset_list(model):
+    table = model.schemas['PDB'].tables['ihm_dataset_list']
+
+    table.create_key(
+        Key.define(["RID", "structure_id", "id"], constraint_names=[["PDB", "ihm_dataset_list_combo1_key"]] )
+    )
+
+# ---------------
 def update_PDB_ihm_related_datasets(model):
     table = model.schemas['PDB'].tables['ihm_related_datasets']
     
@@ -860,6 +968,30 @@ def update_PDB_ihm_related_datasets(model):
                             constraint_names=[ ["PDB", "ihm_related_datasets_ihm_data_transformation_combo2_fkey"] ],
                             on_update="CASCADE",
                             on_delete="NO ACTION")  # won't allow delete until there is no reference
+        )
+
+# ---------------
+def update_PDB_ihm_derived_distance_restraint(model):
+    table = model.schemas['PDB'].tables['ihm_derived_distance_restraint']
+    
+    # -- add columns
+    if 'distance_threshold_mean' not in table.columns.elements:
+        table.create_column(
+            Column.define(
+                'distance_threshold_mean',
+                builtin_types.float8,
+                comment='The distance threshold mean applied to the restraint',
+                nullok=True
+            )
+        )
+    if 'distance_threshold_esd' not in table.columns.elements:
+        table.create_column(
+            Column.define(
+                'distance_threshold_esd',
+                builtin_types.float8,
+                comment='The estimated standard deviation of the distance threshold applied to the restraint',
+                nullok=True
+            )
         )
 # ========================================================
 # add rows to Vocab.ihm_cross_link_list_linker_type table
@@ -964,7 +1096,8 @@ def main(server_name, catalog_id, credentials):
     #create_table_if_not_exist(model, "Vocab",  define_Vocab_table('pseudo_site_flag', 'Flag for crosslinks involving pseudo sites'))
     #create_table_if_not_exist(model, "PDB",  define_tdoc_ihm_ensemble_sub_sample())
     #create_table_if_not_exist(model, "PDB",  define_tdoc_ihm_cross_link_pseudo_site())
-    create_table_if_not_exist(model, "PDB", define_tdoc_ihm_data_transformation()) 
+    #create_table_if_not_exist(model, "PDB", define_tdoc_ihm_data_transformation()) 
+    create_table_if_not_exist(model, "PDB", define_tdoc_ihm_hdx_restraint())
 
     # -- update existing tables
     #update_PDB_ihm_feature_list(model)
@@ -972,7 +1105,9 @@ def main(server_name, catalog_id, credentials):
     #update_PDB_ihm_cross_link_restraint(model)
     #update_PDB_ihm_model_list(model)
     #update_PDB_ihm_ensemble_info(model)
-    update_PDB_ihm_related_datasets(model)
+    #update_PDB_ihm_related_datasets(model)
+    #update_PDB_ihm_derived_distance_restraint(model)
+    #update_PDB_ihm_dataset_list(model)
 
     # Rename existing keys
     #rename_key(model, 'PDB', 'ihm_model_group', 'ihm_model_group_RID_id_key', 'ihm_model_group_combo2_key')
