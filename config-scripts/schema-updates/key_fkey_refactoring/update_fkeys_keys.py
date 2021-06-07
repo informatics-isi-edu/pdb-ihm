e66579b131f370ea02cb8233062d8a91b20b3912
Hongsuda Tangmunarunkit <hongsuda@isi.edu>
2021-06-07 13:29:20 -0700

    Updated the script to selectively printout differens fkey types

--- config-scripts/schema-updates/key_fkey_refactoring/update_fkeys_keys.py
+++ config-scripts/schema-updates/key_fkey_refactoring/update_fkeys_keys.py
@@ -48,17 +48,42 @@ def print_keys_with_rid(model):
                         print("-- ** [%d] %s %s => %s : %s" % (key_length, table.name, key.constraint_name, key_name, set([ c.name for c in key.unique_columns ])))
 
 
-def print_fkeys_with_rid(model, ncols, exclude_deriva=True):
+# given a primary key, get equivalent combo fkey 
+def get_combo_key(model, combo, fkey):
+    fkey_parent_col_names = {c.name for c in fkey.column_map.values()}
+    fkey_col_names = {c.name for c in fkey.column_map.keys()}
+    pk_table = fkey.pk_table
+    
+    # TO BE FILLED
+
+'''paramters:    
+   model: relevant ermrest model 
+   ncols: number of fkey columns to be investigated
+   deriva_included: whether to include fkeys of deriva-related table (i.e. non-mmcif tables)
+   combo1_included: whether to include combo1 fkeys in the logic
+   combo2_included: whether to include combo2 fkeys in the logic
+   primary_types: the types of primary fkeys to be included. If empty, no primary fkeys will be included. 
+'''
+def print_fkeys_with_rid(model, ncols, deriva_included=False, combo1_included=True, combo2_included=True, primary_types=('combo1', 'combo2')):
     schema = model.schemas["PDB"]
     deriva_tables = {'Catalog_Group', 'ERMrest_Client', 'Entry_Related_File'}
+    combo1_count = 1
+    combo2_count = 1
+    primary_count = 1
     
-    for table in schema.tables.values():
+
+    print("=========== print_fkeys_with_rid with ncols = %d ================= " % (ncols))
+    for table in sorted(schema.tables.values(), key = lambda x: x.name):
+        
+        #if table.name == 'ihm_cross_link_pseudo_site':
+            #print("********* TABLE NAME = %s fkeys: %s" % (table.name, table.foreign_keys))
+        
         for fkey in table.foreign_keys:
             fkey_length = len(fkey.columns)
             fkey_parent_col_names = {c.name for c in fkey.column_map.values()}
             fkey_col_names = {c.name for c in fkey.column_map.keys()}
             pk_table = fkey.pk_table
-            
+
             # only look at fkeys to PDB schema (not Vocab)
             if fkey.pk_table.schema.name != 'PDB':
                 continue
@@ -66,36 +91,49 @@ def print_fkeys_with_rid(model, ncols, exclude_deriva=True):
             # focus on a particular number of columns
             if (fkey_length != ncols):
                 continue
+
+            #print("table name = %s, fkey_col_names = %s, parent_col_names = %s" % (table.name, fkey_col_names, fkey_parent_col_names))
             
             # combo1 or combo2 fkeys
             if ('RID' in fkey_parent_col_names):
+                
                 # check for column_name corresponding to RID in the parent table
                 for from_col, to_col in fkey.column_map.items():
                     if to_col.name == 'RID':
                         primary_col_names = fkey_col_names - {from_col.name}
-                        
+
+
                 # -- combo1 
                 if ('structure_id' in fkey_parent_col_names):
+                    if not combo1_included:
+                        continue
                     # check whether primiary key still exists                    
                     if list(table.fkeys_by_columns(primary_col_names, raise_nomatch=False)):                    
                         print("-c1  p  [%d] %s -> %s : %s : %s -> %s" % (fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names))
                         # TODO: for ncols=2, delete corresponding primary key from the model. Somehow some are deleted but some are still present. 
                     else:
                         print("-c1      [%d] %s -> %s : %s : %s -> %s" % (fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names))
+                    combo1_count = combo1_count + 1
                 # -- combo2 case                
                 else:
+                    if not combo2_included:
+                        continue
                     primary_col_names = primary_col_names | {'structure_id'}
                     # check whether primiary key still exists                                        
                     if list(table.fkeys_by_columns(primary_col_names, raise_nomatch=False)):
-                        print("-c2  p  [%d] %s -> %s : %s : %s -> %s --> p%s" % (fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names, primary_col_names))
+                        print("%2d: -c2  p  [%d] %s -> %s : %s : %s -> %s --> p%s" % (combo2_count, fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names, primary_col_names))
                         # TODO: for ncols=2, delete corresponding primary key from the model. Somehow some are deleted but some are still present. 
                     else:
-                        print("-c2      [%d] %s -> %s : %s : %s -> %s -- p%s" % (fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names, primary_col_names))
+                        print("%2d: -c2     [%d] %s -> %s : %s : %s -> %s -- p%s" % (combo2_count, fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names, primary_col_names))
+                    combo2_count = combo2_count + 1
                     
             # primary key with structure_id or other kinds
             else :
                 # -- normal composite key
                 if ('structure_id' in fkey_parent_col_names):
+                    
+                    if not primary_types:
+                        continue
                     # TODO: create a lookup table for long table names
                     # can't check for combo1/2 since the parent_table RID column is not known. Some tables have shorter name lookup
                     
@@ -123,6 +161,10 @@ def print_fkeys_with_rid(model, ncols, exclude_deriva=True):
                     expected_fkey_constraint_name = '_'.join([fkey.table.name, pk_table.name, 'combo'+flag, 'fkey'])
                     expected_parent_key_name = '_'.join([pk_table.name, 'combo'+flag, 'key'])
 
+                    # -- selectively omit primary fkeys investigation depending on the flag. This help with the printout readability
+                    if 'combo'+flag not in primary_types: 
+                        continue
+                    
                     # 3. check whether the combo fkeys already exist.
                     found = False                    
                     for fk in table.foreign_keys:
@@ -138,12 +180,14 @@ def print_fkeys_with_rid(model, ncols, exclude_deriva=True):
                         if 'RID' in fk_parent_col_names:
                             if fk_parent_col_names == expected_fkey_parent_column_names:
                                 found = True
-                                print("--p  c%s [%d] %s -> %s : %s : %s -> %s" % (flag, fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names))
+                                print("%2d: --p  c%s [%d] %s -> %s : %s : %s -> %s" % (primary_count, flag, fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names))
                             else:
                                 print("ERROR: found 'RID' fkey but not combo%s in %s -> %s : %s : %s -> %s" % (flag, table.name, pk_table.name, fk.constraint_name, {c.name for c in fk.column_map.keys()}, {c.name for c in fk.column_map.values()}))
 
                     if not found:
-                        print("--p      [%d] %s -> %s : %s : %s -> %s" % (fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names))
+                        print("%2d: --p      [%d] %s -> %s : %s : %s -> %s" % (primary_count, fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names))
+
+                    primary_count = primary_count + 1
                     
                     # 4. check whether there are multiple fkeys pointing to the same parent table.
                     dup_fkey_parent = False
@@ -179,7 +223,7 @@ def print_fkeys_with_rid(model, ncols, exclude_deriva=True):
                         
                 # -- single keys or composite keys without structure_id (which should be none)
                 else:
-                    if fkey.pk_table.name not in deriva_tables or exclude_deriva == False:
+                    if fkey.pk_table.name not in deriva_tables or deriva_included == True:
                         print("---       [%d] %s -> %s : %s : %s -> %s" % (fkey_length, table.name, fkey.pk_table.name, fkey.constraint_name, fkey_col_names, fkey_parent_col_names))
 
 
@@ -199,7 +243,10 @@ def main(server_name, catalog_id, credentials):
     catalog.dcctx['cid'] = "oneoff/model"
     model = catalog.getCatalogModel()
 
-    print_fkeys_with_rid(model, 2)
+    
+    print_fkeys_with_rid(model, 2, combo1_included=False, combo2_included=False, primary_types=("combo2"))
+    
+    print_fkeys_with_rid(model, 4, combo1_included=False, combo2_included=True, primary_types=("combo1"))
  
         
 
