a6aa0c45be67dd141ee03520cc0e090eb11ec904
serban <serban@localhost>
2022-04-15 14:40:52 -0700

    Added scripts to set UNIQUE constraints for the accession code and to set update ACLs to the Entry_mmCIF_File table for pdb_admin, isrd_staff and pdb_curator.

---
+++ config-scripts/schema-updates/accesion_code/2022_10_set_unique_accession.py
@@ -0,0 +1,24 @@
+import sys
+import json
+from deriva.core import ErmrestCatalog, AttrDict, get_credential, DEFAULT_CREDENTIAL_FILE, tag, urlquote, DerivaServer, get_credential, BaseCLI
+from deriva.core.ermrest_model import builtin_types, Schema, Table, Column, Key, ForeignKey, DomainType, ArrayType
+import utils
+import re
+
+# ============================================================
+def main(server_name, catalog_id, credentials):
+    server = DerivaServer('https', server_name, credentials)
+    catalog = server.connect_ermrest(catalog_id)
+    catalog.dcctx['cid'] = 'oneoff/model'
+    model = catalog.getCatalogModel()
+    
+    utils.create_key_if_not_exists(model, 'PDB', 'entry', ['Accession_Serial'], 'entry_Accession_Serial_primary_key')
+    utils.create_key_if_not_exists(model, 'PDB', 'entry', ['accession_code'], 'entry_accession_code_primary_key')
+    
+# ===================================================    
+
+if __name__ == '__main__':
+    args = BaseCLI('ad-hoc table creation tool', None, 1).parse_cli()
+    credentials = get_credential(args.host, args.credential_file)
+    main(args.host, 1, credentials)
+    
