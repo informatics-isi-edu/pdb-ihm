3758d1f8d486de775219313ecd670ec91bf09381
serban <serban@localhost>
2023-10-05 15:26:22 -0700

    Added script for updating the configuration files.

---
+++ scripts/pdb_update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp /home/isrddev/protein-database/scripts/pdb_processing/config/cif.json /home/pdbihm/pdb/config/dev/
+cp /home/isrddev/protein-database/scripts/pdb_processing/config/cif.json /home/pdbihm/pdb/config/staging/
+cp /home/isrddev/protein-database/scripts/validator/CifCheck /home/pdbihm/pdb/cpp-dict-pack/build/bin/
+cp /home/isrddev/protein-database/scripts/make-mmCIF/make-mmcif.py /home/pdbihm/pdb/make-mmCIF/
+cp /home/isrddev/protein-database/scripts/make-json/py-rcsb_db/rcsb/db/tests-validate/testSchemaDataPrepValidate-ihm.py /home/pdbihm/pdb/py-rcsb_db/rcsb/db/tests-validate/
+cp /home/isrddev/protein-database/scripts/www-config/default/chaise/chaise-config.js /var/www/html/chaise/
