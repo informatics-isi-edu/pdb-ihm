9d72772102126e1e9e0d019b4ab4bd09d9ffe6f7
serban <serban@localhost>
2023-10-19 11:18:56 -0700

    Updated the pdb_update.sh script.

--- scripts/pdb_update.sh
+++ scripts/pdb_update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cp /home/isrddev/protein-database/scripts/pdb_processing/config/ermrest_table_defs.json /home/pdbihm/pdb/config/dev/
-cp /home/isrddev/protein-database/scripts/pdb_processing/config/ermrest_table_defs.json /home/pdbihm/pdb/config/staging/
+cp /home/isrddev/protein-database/scripts/pdb_processing/config/mmcif_tables_input2output.json /home/pdbihm/pdb/config/dev/
+cp /home/isrddev/protein-database/scripts/pdb_processing/config/mmcif_tables_input2output.json /home/pdbihm/pdb/config/staging/
 cp /home/isrddev/protein-database/scripts/validator/CifCheck /home/pdbihm/pdb/cpp-dict-pack/build/bin/
 cp /home/isrddev/protein-database/scripts/make-mmCIF/make-mmcif.py /home/pdbihm/pdb/make-mmCIF/
 cp /home/isrddev/protein-database/scripts/make-json/py-rcsb_db/rcsb/db/tests-validate/testSchemaDataPrepValidate-ihm.py /home/pdbihm/pdb/py-rcsb_db/rcsb/db/tests-validate/
