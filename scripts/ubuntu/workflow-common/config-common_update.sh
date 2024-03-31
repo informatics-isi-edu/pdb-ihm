97dfbd3375fb059b5705aeef72857c1b0355524a
serbanvoinea <serbanvoinea@192.168.1.249>
2024-03-30 18:37:03 -0700

    Updated the Ubuntu config for workflow.

--- scripts/ubuntu/workflow-common/config-common_update.sh
+++ scripts/ubuntu/workflow-common/config-common_update.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-cp /home/isrddev/protein-database/scripts/make-json/py-rcsb_db/rcsb/db/config/exdb-config-example-ihm-DEPO.yml /home/pdbihm/pdb/py-rcsb_db/rcsb/db/config/
-cp /home/isrddev/protein-database/scripts/make-json/py-rcsb_db/rcsb/db/config/exdb-config-example-ihm-HOLD-REL.yml /home/pdbihm/pdb/py-rcsb_db/rcsb/db/config/
-cp /home/isrddev/protein-database/scripts/validator/CifCheck /home/pdbihm/pdb/cpp-dict-pack/build/bin/
-cp /home/isrddev/protein-database/scripts/make-mmCIF/make-mmcif.py /home/pdbihm/pdb/make-mmCIF/
-cp /home/isrddev/protein-database/scripts/make-json/py-rcsb_db/rcsb/db/tests-validate/testSchemaDataPrepValidate-ihm.py /home/pdbihm/pdb/py-rcsb_db/rcsb/db/tests-validate/
+cp /home/isrddev/python-ihm/util/make-mmcif.py /home/pdbihm/pdb/make-mmCIF/
