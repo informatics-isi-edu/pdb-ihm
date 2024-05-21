4eb76462958967968ef096a3dd2e252ce0aa75e5
serbanvoinea <serbanvoinea@localhost>
2024-05-21 09:41:22 -0700

    Updated the pdb_update.sh script.

--- scripts/pdb_update.sh
+++ scripts/pdb_update.sh
@@ -5,4 +5,4 @@ cp /home/isrddev/protein-database/scripts/pdb_processing/config/mmcif_tables_inp
 cp /home/isrddev/protein-database/scripts/validator/CifCheck /home/pdbihm/pdb/cpp-dict-pack/build/bin/
 cp /home/isrddev/protein-database/scripts/make-mmCIF/make-mmcif.py /home/pdbihm/pdb/make-mmCIF/
 cp /home/isrddev/protein-database/scripts/make-json/py-rcsb_db/rcsb/db/tests-validate/testSchemaDataPrepValidate-ihm.py /home/pdbihm/pdb/py-rcsb_db/rcsb/db/tests-validate/
-cp /home/isrddev/protein-database/scripts/www-config/dev-aws.pdb-dev.org/chaise/config/chaise-config.js /var/www/html/chaise/
+cp /home/isrddev/protein-database/scripts/www-config/dev-aws.pdb-dev.org/chaise/config/chaise-config.js /var/www/html/chaise/config/
