957ef4aec25aff070a6989034d8ec479a7dc6a5e
serbanvoinea <serbanvoinea@localhost>
2024-07-19 13:32:23 -0700

    Ignore columns that are not in ermrest.

--- pdb_dev/config/app/get_ucode_all.py
+++ pdb_dev/config/app/get_ucode_all.py
@@ -71,6 +71,7 @@ ucode = {}
 
 vocab_tables = []
 
+missing_columns = {}
 missing_tables = []
 
 for input in ['ucode_ihm.log', 'ucode_mmCIF.log']:
@@ -101,11 +102,18 @@ for table_name,columns in ucode.items():
         table = model_root.schemas[schema_name].tables[table_name]
         fkeys = table.foreign_keys
         for column_name in columns:
-            column = table.column_definitions.__getitem__(column_name)
-            column_is_fk(fkeys, table_name, column_name, model_root, '', vocab_tables)
+            try:
+                column = table.column_definitions.__getitem__(column_name)
+                column_is_fk(fkeys, table_name, column_name, model_root, '', vocab_tables)
+            except:
+                if table_name not in missing_columns.keys():
+                    missing_columns[table_name] = []
+                missing_columns[table_name].append(column_name)
     except:
         missing_tables.append(table_name)
 
+print(json.dumps(missing_tables, indent=4))
+print(json.dumps(missing_columns, indent=4))
 if False:
     for vocab_table in vocab_tables:
         print('select count(*) as {} from "Vocab".{};'.format(vocab_table, vocab_table))
