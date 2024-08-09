ceff1a4b1c6234fbc555187fe6758a97ff6b3914
Hongsuda Tangmunarunkit <hongsuda@isi.edu>
2024-08-09 12:50:56 -0700

    Updated and rename archive related scripts to use new archive_processing namespace

--- scripts/cron/crontab/pdb_gen_archive_production.sh
+++ scripts/cron/crontab/pdb_gen_archive_production.sh
@@ -6,5 +6,5 @@
 # -------   
 
 cd /home/pdbihm
-sudo -u pdbihm python3 -m pdb_dev.archive.client --config /home/pdbihm/pdb/config/www/pdb_archive.json
-
+#sudo -u pdbihm python3 -m pdb_dev.archive.client --config /home/pdbihm/pdb/config/www/pdb_archive.json
+sudo -u pdbihm python3 -m pdb_dev.processing.archive.client --config /home/pdbihm/pdb/config/archive_processing/pdb_archive.json --host data.pdb-dev.org --catalog-id 1
