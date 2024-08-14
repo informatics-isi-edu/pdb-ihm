a11cf2e38be62f1dd2873dac81557426ffa5c74b
serbanvoinea <serbanvoinea@localhost>
2024-08-13 22:48:13 -0700

    Added host parameter to the production archive file.

--- scripts/cron/crontab/pdb_gen_archive_production.sh
+++ scripts/cron/crontab/pdb_gen_archive_production.sh
@@ -7,4 +7,4 @@
 
 cd /home/pdbihm
 #sudo -u pdbihm python3 -m pdb_dev.archive.client --config /home/pdbihm/pdb/config/www/pdb_archive.json
-sudo -u pdbihm python3 -m pdb_dev.processing.archive.client --config /home/pdbihm/pdb/config/www/pdb_archive.json --catalog-id 1
+sudo -u pdbihm python3 -m pdb_dev.processing.archive.client --config /home/pdbihm/pdb/config/www/pdb_archive.json --host data.pdb-dev.org --catalog-id 1
