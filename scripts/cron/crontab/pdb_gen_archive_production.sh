46ea7e0ae4117a1331934a18f1a836dc97cbb5d5
Hongsuda Tangmunarunkit <hongsuda@isi.edu>
2024-10-23 18:26:42 -0700

    Update archive related scripts for production

--- scripts/cron/crontab/pdb_gen_archive_production.sh
+++ scripts/cron/crontab/pdb_gen_archive_production.sh
@@ -6,7 +6,6 @@
 # -------   
 
 cd /home/pdbihm
-#sudo -u pdbihm python3 -m pdb_dev.archive.client --config /home/pdbihm/pdb/config/www/pdb_archive.json
-sudo -u pdbihm python3 -m pdb_dev.processing.archive.client --config /home/pdbihm/pdb/config/www/pdb_archive.json --host data.pdb-dev.org --catalog-id 1
+sudo -u pdbihm python3 -m pdb_dev.processing.archive.client --config /home/pdbihm/config/archive_processing/pdb_archive_config.json --host data.pdb-dev.org --catalog-id 1
 
 python3 -m pdb_dev.tools.send_email_notification --config /home/pdbihm/.secrets/mail.json -s "Archive Generation has Completed" -b ""
